3db4cc16c298b9eb81e16c0d2bafa5abf64b7487
Ivar Nilsen <post@ivarnilsen.com>
2018-06-10 10:48:06 +0200

    Level 3

--- warriorjs/ivar-beginner/Player.js
+++ warriorjs/ivar-beginner/Player.js
@@ -7,11 +7,23 @@ class Player {
         return !this.warrior.feel().isEmpty();
     }
 
+    healthIsLow() {
+        return this.warrior.health() < 15;
+    }
+
     playTurn(warrior) {
         this.warrior = warrior;
 
+        if (!this.health) {
+            this.health = warrior.health();
+        }
+
         if (this.enemyInFront()) {
             return this.act(warrior.attack);
+        }
+
+        if (this.healthIsLow()) {
+            return this.act(warrior.rest);
         }
 
         return this.act(warrior.walk);
